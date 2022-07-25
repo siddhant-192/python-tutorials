e5643699aa71b18a3fc8336bc36070a5c554fc73
siddhant-192 <107173442+siddhant-192@users.noreply.github.com>
2022-07-25 09:21:33 +0530

    Update Tutorial 02_01.py

--- Tutorial 02_01.py
+++ Tutorial 02_01.py
@@ -1,50 +1,12 @@
-#A cloth showroom has announced the following discounts on the purchase of specific items:
-#Amount Shorts Pants Shits/T-Shirts
-#0-100 – 3% 5%
-#101-200 5% 8% 10%
-#201-300 10% 12% 15%
-#Above 300 18% 20% 22%
-#Ask user to enter the amount and assign following code for the items such as s for shorts, p for pans and t for shirts/t-shirts.
-#Compute the discount and net amount paid by customer.
+#Accept three sides of a triangle and print if the triangle is equilateral, isosceles or scalene. 
 
-cloth=input("Enter cloth type : ")
-amt=int(input("Enter amount (s p t) : "))
+a=int(input("Enter side a : "))
+b=int(input("Enter side b : "))
+c=int(input("Enter side c : "))
 
-if amt>=0 and amt<=100:
-    if cloth=='s':
-        bill=amt*1
-    elif cloth=='p':
-        bill=amt*0.97
-    elif cloth=='t':
-        bill=amt*0.95
-    else:
-        print("Invalid Input")
-elif amt>=101 and amt<=200:
-    if cloth=='s':
-        bill=amt*0.95
-    elif cloth=='p':
-        bill=amt*0.92
-    elif cloth=='t':
-        bill=amt*0.9
-    else:
-        print("Invalid Input")
-elif amt>=201 and amt<=300:
-    if cloth=='s':
-        bill=amt*0.9
-    elif cloth=='p':
-        bill=amt*0.88
-    elif cloth=='t':
-        bill=amt*0.85
-    else:
-        print("Invalid Input")
-elif amt>300:
-    if cloth=='s':
-        bill=amt*0.82
-    elif cloth=='p':
-        bill=amt*0.8
-    elif cloth=='t':
-        bill=amt*0.78
-    else:
-        bill="Invalid Input"
-
-print(bill)
+if a==b==c:
+    print("Equilateral")
+elif a==b or b==c or c==a:
+    print("Isosceles")
+else:
+    print("Scalene")
